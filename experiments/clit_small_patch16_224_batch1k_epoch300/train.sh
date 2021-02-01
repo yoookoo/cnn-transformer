9bd8e127b7fed37b1277f7cd739bbec1359b533b
yuankun <yuankun@sensetime.com>
2021-02-01 22:09:44 +0800

    add configs

---
+++ experiments/clit_small_patch16_224_batch1k_epoch300/train.sh
@@ -0,0 +1,8 @@
+PYTHONPATH=$PYTHONPATH:../../ \
+srun --mpi=pmi2 -p $1 -n$2 --gres=gpu:8 --ntasks-per-node=8 --cpus-per-task=5 \
+python -u -m main \
+    --model clit_small_patch16_224 --local-size 3 --local-with-bn \
+    --batch-size 64 \
+    --data-path /mnt/lustre/share/images \
+    --output_dir ./ --dist-eval \
+    2>&1 | tee log.txt
